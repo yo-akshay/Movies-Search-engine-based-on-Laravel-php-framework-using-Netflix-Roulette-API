478b8c6fb77f050c9f0677c807e3eee95b69c8ea
yo-akshay <akshay1234530@yahoo.com>
2017-03-12 11:09:23 -0700

    Update search_engine.php

--- app/Http/Controllers/search_engine.php
+++ app/Http/Controllers/search_engine.php
@@ -79,47 +79,59 @@ class search_engine extends Controller
 
 						// convert response
 						$output = json_decode($output);
-						foreach($output as $value)
+						if(key($output)=="errorcode")
 						{
-							
-							foreach($value as $key =>$row)
+							foreach($output as $key =>$row)
 							{
-								if($key == "mediatype" || $key == "runtime" || $key == "unit" || $key == "show_id" )
-									continue;
-								else if($key == "show_cast" )
+									echo "<p>$key  :  $row";
+										echo '<br>';
+							}
+						}
+						else
+						{
+							foreach($output as $value)
+							{
+								
+								foreach($value as $key =>$row)
 								{
-									echo"<br>Show Cast:";
-									$pieces = explode(",", $row);
-									foreach($pieces as $strings)
+									if($key == "mediatype" || $key == "runtime" || $key == "unit" || $key == "show_id" )
+										continue;
+									else if($key == "show_cast" )
 									{
-										$link='http://localhost:8000/?Title=&director=&Actor='.$strings;
-										echo "<li><a href='$link'>$strings</a>";
+										echo"<br>Show Cast:";
+										$pieces = explode(",", $row);
+										foreach($pieces as $strings)
+										{
+											$link='http://localhost:8000/?Title=&director=&Actor='.$strings;
+											echo "<li><a href='$link'>$strings</a>";
+										}
+										echo "<br>";
 									}
-									echo "<br>";
-								}
-								else if($key == "director" )
-									{
-										if(empty($row))
-										echo"<br>Director:No details of director<br>";
+									else if($key == "director" )
+										{
+											if(empty($row))
+											echo"<br>Director:No details of director<br>";
+											else
+											{
+												echo"<br>Director:";
+												$link='http://localhost:8000/?Title=&director='.$row.'&Actor=';
+												echo "<a href='$link'>$row</a><br>";
+											}
+										}
+										else if($key !="poster")
+										{
+											echo "$key  :  $row";
+												echo '<br>';
+										}
 										else
 										{
-											echo"<br>Director:";
-											$link='http://localhost:8000/?Title=&director='.$row.'&Actor=';
-											echo "<a href='$link'>$row</a><br>";
+											echo '<img src="'.$row.'" />';
 										}
-									}
-									else if($key !="poster")
-									{
-										echo "$key  :  $row";
-											echo '<br>';
-									}
-									else
-									{
-										echo '<img src="'.$row.'" />';
-									}
+								}
+								echo "<br><br><br><br><br><br>";
 							}
-							echo "<br><br><br><br><br><br>";
 						}
+						
 						curl_close($ch);
 					}
 					else if(!empty($Actor))
@@ -132,47 +144,58 @@ class search_engine extends Controller
 						// convert response
 						$output = json_decode($output);
 						echo "<br><br>";
-						foreach($output as $value)
+						if(key($output)=="errorcode")
 						{
-							
-							foreach($value as $key =>$row)
+							foreach($output as $key =>$row)
 							{
-								if($key == "mediatype" || $key == "runtime" || $key == "unit" || $key == "show_id" )
-									continue;
-								else if($key == "show_cast" )
+									echo "<p>$key  :  $row";
+										echo '<br>';
+							}
+						}
+						else
+						{
+							foreach($output as $value)
+							{
+								
+								foreach($value as $key =>$row)
 								{
-									echo"<br>Show Cast:";
-									$pieces = explode(",", $row);
-									foreach($pieces as $strings)
+									if($key == "mediatype" || $key == "runtime" || $key == "unit" || $key == "show_id" )
+										continue;
+									else if($key == "show_cast" )
 									{
-										$link='http://localhost:8000/?Title=&director=&Actor='.$strings;
-										echo "<li><a href='$link'>$strings</a>";
+										echo"<br>Show Cast:";
+										$pieces = explode(",", $row);
+										foreach($pieces as $strings)
+										{
+											$link='http://localhost:8000/?Title=&director=&Actor='.$strings;
+											echo "<li><a href='$link'>$strings</a>";
+										}
+										echo "<br>";
 									}
-									echo "<br>";
-								}
-								else if($key == "director" )
-									{
-										if(empty($row))
-										echo"<br>Director:No details of director<br>";
+									else if($key == "director" )
+										{
+											if(empty($row))
+											echo"<br>Director:No details of director<br>";
+											else
+											{
+												echo"<br>Director:";
+												$link='http://localhost:8000/?Title=&director='.$row.'&Actor=';
+												echo "<a href='$link'>$row</a><br>";
+											}
+										}
+										else if($key !="poster")
+										{
+											echo "$key  :  $row";
+												echo '<br>';
+										}
 										else
 										{
-											echo"<br>Director:";
-											$link='http://localhost:8000/?Title=&director='.$row.'&Actor=';
-											echo "<a href='$link'>$row</a><br>";
+											echo '<img src="'.$row.'" />';
 										}
-									}
-									else if($key !="poster")
-									{
-										echo "$key  :  $row";
-											echo '<br>';
-									}
-									else
-									{
-										echo '<img src="'.$row.'" />';
-									}
+								}
+								
+								echo "<br><br><br><br><br><br>";
 							}
-							
-							echo "<br><br><br><br><br><br>";
 						}
 						curl_close($ch);
 					}
